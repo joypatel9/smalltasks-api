d4a224a58d064136b017d12d86a149efc8bb754a
Joy Patel <joypatel280@gmail.com>
2021-06-13 12:18:57 +0530

    #4: JPA Auditing configuration

--- src/main/java/com/joypatel/smalltasks/SmalltasksApiApplication.java
+++ src/main/java/com/joypatel/smalltasks/SmalltasksApiApplication.java
@@ -2,8 +2,10 @@ package com.joypatel.smalltasks;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
 @SpringBootApplication
+@EnableJpaAuditing
 public class SmalltasksApiApplication {
 
     public static void main(String[] args) {
--- src/main/java/com/joypatel/smalltasks/common/domain/AbstractEntity.java
+++ src/main/java/com/joypatel/smalltasks/common/domain/AbstractEntity.java
@@ -30,6 +30,7 @@ public abstract class AbstractEntity<ID extends Serializable> {
 
     @CreatedDate
     @Temporal(TemporalType.TIMESTAMP)
+    @Column(nullable = false, columnDefinition = "TIMESTAMP WITH TIME ZONE")
     private Date createdDate;
 
     @LastModifiedBy
@@ -37,6 +38,7 @@ public abstract class AbstractEntity<ID extends Serializable> {
 
     @LastModifiedDate
     @Temporal(TemporalType.TIMESTAMP)
+    @Column(nullable = false, columnDefinition = "TIMESTAMP WITH TIME ZONE")
     private Date lastModifiedDate;
 
     @Version
